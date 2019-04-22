0747dd7ffc24d1ba00ec4b3ca0d16c002742069b
lturmel <lturmel@expertasolutions.com>
2019-04-21 23:13:32 -0400

    get selector name/value
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

---
+++ KubectlGetServiceSelectors/src/aksGetServiceSelector.js
@@ -0,0 +1,103 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+
+const tl = require("vsts-task-lib/task");
+const path = require("path");
+
+const clusterconnection_1 = require("./clusterconnection");
+tl.setResourcePath(path.join(__dirname, '..', 'task.json'));
+// Change to any specified working directory
+tl.cd(tl.getInput("cwd"));
+
+var registryType = "Azure Container Registry";
+const environmentVariableMaximumSize = 32766;
+
+var command = "get";
+
+var kubeconfigfilePath = "";
+if (command === "logout") {
+    kubeconfigfilePath = tl.getVariable("KUBECONFIG");
+}
+
+var connection = new clusterconnection_1.default(kubeconfigfilePath);
+
+try {
+    console.log(connection);
+    connection.open()
+        .then(() => {
+        return run(connection, command);
+})
+.then(() => {
+        tl.setResult(tl.TaskResult.Succeeded, "");
+    if (command !== "login") {
+        connection.close();
+    }
+}).catch((error) => {
+        tl.setResult(tl.TaskResult.Failed, error.message);
+    connection.close();
+});
+}
+catch (error) {
+    tl.setResult(tl.TaskResult.Failed, error.message);
+}
+
+function run(clusterConnection, command) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("inside it");
+        var targetServiceName = tl.getInput("targetService", true);
+        console.log("targetService: " + targetServiceName);
+
+        console.log("Pod Service Ip not found, still looking")
+        yield executeKubectlCommand(clusterConnection, "get", "service " + targetServiceName)
+            .then(function() {
+                var podService = tl.getVariable('podServiceContent');
+                let json = JSON.parse(podService);
+                tl.setVariable("selectorValue", "invalid");
+            });
+    });
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+            break;
+        }
+    }
+}
+
+// execute kubectl command
+function executeKubectlCommand(clusterConnection, command, args) {
+    var commandMap = {
+        "login": "./kuberneteslogin",
+        "logout": "./kuberneteslogout"
+    };
+    var commandImplementation = require("./kubernetescommand");
+    if (command in commandMap) {
+        commandImplementation = require(commandMap[command]);
+    }
+    var telemetry = {
+        registryType: registryType,
+        command: command
+    };
+    console.log("##vso[telemetry.publish area=%s;feature=%s]%s", "TaskEndpointId", "KubernetesV1", JSON.stringify(telemetry));
+    var result = "";
+    return commandImplementation.run(clusterConnection, command, args, (data) => result += data)
+        .fin(function cleanup() {
+                var commandOutputLength = result.length;
+                if (commandOutputLength > environmentVariableMaximumSize) {
+                    tl.warning(tl.loc("OutputVariableDataSizeExceeded", commandOutputLength, environmentVariableMaximumSize));
+                }
+                else {
+                    tl.setVariable('podServiceContent', result);
+                }
+            });
+}
---
+++ KubectlGetServiceSelectors/src/clusters/armkubernetescluster.js
@@ -0,0 +1,46 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const tl = require("vsts-task-lib/task");
+const azure_arm_aks_service_1 = require("azure-arm-rest/azure-arm-aks-service");
+const azure_arm_endpoint_1 = require("azure-arm-rest/azure-arm-endpoint");
+
+// get kubeconfig file content
+function getKubeConfigFromAKS(azureSubscriptionEndpoint, resourceGroup, clusterName) {
+    console.log("getKubeConfigFromAKS");
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("inside awaiter");
+        console.log(azureSubscriptionEndpoint);
+        var azureEndpoint = yield (new azure_arm_endpoint_1.AzureRMEndpoint(azureSubscriptionEndpoint)).getEndpoint();
+        console.log("azureEndPoint *****");
+        console.log(azureEndpoint);
+        var aks = new azure_arm_aks_service_1.AzureAksService(azureEndpoint);
+        console.log("aks *****");
+        console.log(aks);
+        var clusterInfo = yield aks.getAccessProfile(resourceGroup, clusterName);
+        console.log("clusterInfo *****");
+        console.log(clusterInfo);
+        console.log("clusterInfo.properties.kubeConfig");
+        console.log(clusterInfo.properties.kubeConfig);
+        var base64Kubeconfig = Buffer.from(clusterInfo.properties.kubeConfig, 'base64');
+        return base64Kubeconfig.toString();
+    });
+}
+
+function getKubeConfig() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("getKubeConfig");
+        var azureSubscriptionEndpoint = tl.getInput("azureSubscriptionEndpoint", true);
+        var resourceGroup = tl.getInput("containerAzureResourceGroup", true);
+        var clusterName = tl.getInput("kubernetesCluster", true);
+        return getKubeConfigFromAKS(azureSubscriptionEndpoint, resourceGroup, clusterName);
+    });
+}
+exports.getKubeConfig = getKubeConfig;
